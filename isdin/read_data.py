54fa529ce5902a63a70bfa4d7288ac75e9f636ea
Jose Fonceca <ilbertofjunior@gmail.com>
2023-05-14 15:52:34 +0200

    Adding methods to get timeseries from data

--- isdin/read_data.py
+++ isdin/read_data.py
@@ -32,10 +32,8 @@ def read_csv_data(
             data.dropna(
                 subset=[
                     "user_id",
-                    "user_age",
                     "region",
                     "product_id",
-                    "product_family",
                 ],
                 inplace=True,
             )
@@ -64,3 +62,62 @@ def add_season_from_date(data: pd.DataFrame) -> pd.DataFrame:
     """Add a season column to the data"""
     data["season"] = data["transaction_date"].dt.month % 12 // 3
     return data
+
+
+def get_monthly_timeseries(
+    data: pd.DataFrame,
+) -> Tuple[dict, dict]:
+    """
+    Get monthly timeseries by product_id and region
+    """
+    try:
+        data["product_id"] = (
+            data["product_id"].cat.remove_categories(6.0).dropna()
+        )
+    except ValueError:
+        pass
+
+    product_timeseries = get_timeseries(data, "product_id")
+    region_timeseries = get_timeseries(data, "region")
+    return product_timeseries, region_timeseries
+
+
+def get_daily_timeseries(
+    data: pd.DataFrame,
+) -> Tuple[dict, dict]:
+    """
+    Get daily timeseries by product_id and region
+    """
+    try:
+        data["product_id"] = (
+            data["product_id"].cat.remove_categories(6.0).dropna()
+        )
+    except ValueError:
+        pass
+
+    product_timeseries = get_timeseries(data, "product_id", "D")
+    region_timeseries = get_timeseries(data, "region", "D")
+    return product_timeseries, region_timeseries
+
+
+def get_timeseries(data: pd.DataFrame, column: str, period: str = "M") -> dict:
+    """
+    Get the product count time series
+    """
+    grouped = (
+        data.groupby([data["transaction_date"].dt.to_period(period), column])
+        .size()
+        .reset_index()
+    )
+    grouped.columns = [*grouped.columns[:-1], "count"]
+
+    time_series = {}
+
+    for _, row in grouped.iterrows():
+        date = row["transaction_date"].to_timestamp()
+        if date not in time_series:
+            time_series[date] = {}
+        if row[column] not in time_series[date]:
+            time_series[date][row[column]] = row["count"]
+
+    return time_series
