72eb15552b851c4c1e0f515dd3b842b4c7369f3e
Jose Fonceca <ilbertofjunior@gmail.com>
2023-05-15 23:42:11 +0200

    Renaming read_data file

--- isdin/etl_data.py
+++ isdin/etl_data.py
@@ -1,9 +1,11 @@
 """Script to read the file and transform the columns accordingly"""
 
+from pathlib import Path
+from typing import Tuple
+
 import pandas as pd
 
-from pathlib import Path
-from typing import List, Tuple
+from dateutil.relativedelta import relativedelta
 
 
 def read_csv_data(
@@ -121,3 +123,105 @@ def get_timeseries(data: pd.DataFrame, column: str, period: str = "M") -> dict:
             time_series[date][row[column]] = row["count"]
 
     return time_series
+
+
+def neuralforecast_df_all(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function converts the original data frame into a pandas
+    DataFrame that can be used by neuralforecast classes.
+    """
+    grouped = (
+        data.groupby(
+            [
+                data["transaction_date"].dt.to_period("D"),
+                "product_id",
+                "region",
+            ]
+        )
+        .size()
+        .reset_index()
+    )
+    grouped.columns = [*grouped.columns[:-1], "y"]
+
+    # Changing transaction_date from Period to datetime format
+    grouped["transaction_date"] = grouped["transaction_date"].dt.to_timestamp()
+
+    # Creating the unique_id column
+    grouped["unique_id"] = grouped[["product_id", "region"]].apply(
+        lambda x: f"{x.region}_product_{x.product_id}", axis=1
+    )
+
+    # Renaming and dropping columns
+    grouped["ds"] = grouped["transaction_date"]
+    grouped.drop(
+        columns=["transaction_date", "product_id", "region"], inplace=True
+    )
+
+    # Adding the week day to the data frame
+    return add_weekdays_to_data(grouped)
+
+
+def add_weekdays_to_data(data: pd.DataFrame) -> pd.DataFrame:
+    """Add weekdays boolean columns"""
+    weekday = pd.get_dummies(data["ds"].dt.weekday)
+    weekday.columns = ["weekday_" + str(i) for i in range(weekday.shape[1])]
+    data = pd.concat([data, weekday], axis=1)
+    return data
+
+
+def imput_missing_data(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Input missing data with average value from previous month for a specific
+    weekday
+    """
+    missing_days = pd.date_range(
+        start="2021-06-13", end="2023-02-13"
+    ).difference(data["ds"])
+
+    for missing_day in missing_days:
+        previous_month = missing_day - relativedelta(months=1)
+        weekday = f"weekday_{missing_days[0].weekday()}"
+        averages = (
+            (
+                data[
+                    (data["ds"] >= previous_month)
+                    & (data["ds"] < missing_days[0])
+                    & (data[weekday])
+                ]
+            )
+            .groupby(by=["unique_id"])["y"]
+            .mean()
+            .reset_index()
+        )
+        averages["ds"] = missing_day
+        averages = add_weekdays_to_data(averages)
+        data = pd.concat([data, averages])
+    return data.sort_values(by=["ds"])
+
+
+def neuralforecast_df_region_c(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function converts the original data frame into a pandas
+    DataFrame that can be used by neuralforecast classes. It only uses
+    the region C.
+    """
+    region_c = data[data["region"] == "Region C"]
+    grouped = (
+        region_c.groupby([region_c["transaction_date"].dt.to_period("D")])
+        .size()
+        .reset_index()
+    )
+    grouped.columns = [*grouped.columns[:-1], "y"]
+
+    # Changing transaction_date from Period to datetime format
+    grouped["transaction_date"] = grouped["transaction_date"].dt.to_timestamp()
+
+    # Creating the unique_id column
+    grouped["unique_id"] = "Region C"
+
+    # Renaming and dropping columns
+    grouped["ds"] = grouped["transaction_date"]
+    grouped.drop(columns=["transaction_date"], inplace=True)
+
+    # Adding the week day to the data frame
+    return add_weekdays_to_data(grouped)
